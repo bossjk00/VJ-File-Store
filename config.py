1e56b77a8d1d84f49d672ddd29d21ab8357e3585
bossjk00 <sample2sing@gmail.com>
2025-02-10 23:03:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://envs.sh/v9H.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "Leech_67bot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "Filestore_jkbot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
